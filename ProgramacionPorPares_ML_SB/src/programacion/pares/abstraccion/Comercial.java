321751f794da0669280b2e30615cc6f40751c550
MelanyL <75482552+MelanyL@users.noreply.github.com>
2021-02-26 10:48:11 -0500

    Clase Comercial
    En dicha clase procedí a crear los atributos, el constructor y los métodos getter y setter

--- ProgramacionPorPares_ML_SB/src/programacion/pares/abstraccion/Comercial.java
+++ ProgramacionPorPares_ML_SB/src/programacion/pares/abstraccion/Comercial.java
@@ -1,5 +1,26 @@
 package programacion.pares.abstraccion;
 
-public class Comercial {
+public class Comercial extends Empleado {
+  
+  private double comision;
+	
+	public Comercial(String nombre, int edad, double salario) {
+		super(nombre, edad, salario);
+		this.comision = comision;
+	}
 
+	@Override
+	public boolean plus() {
+		throw new UnsupportedOperationException("Not supported yet.");
+		
+	}
+
+	public double getComision() {
+		return comision;
+	}
+
+	public void setComision(double comision) {
+		this.comision = comision;
+	}
+  
 }
