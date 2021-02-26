3efb2e7c2af8f009d31ddcc6650b9a93f9a943ec
StevenFBarragan <64034066+StevenFBarragan@users.noreply.github.com>
2021-02-26 12:55:48 -0500

    Paquete: programacion.pares.Polimorfismo / Clase: Persona
    En esta clase se agregaron:
    - Los atributos.
    - El constructor.
    - Métodos getters y setters.
    - Método abstracto correr()

--- ProgramacionPorPares_ML_SB/src/programacion/pares/Polimorfismo/Persona.java
+++ ProgramacionPorPares_ML_SB/src/programacion/pares/Polimorfismo/Persona.java
@@ -1,5 +1,21 @@
 package programacion.pares.Polimorfismo;
 
 public abstract class Persona {
-
+	
+	private String nombre;
+	 	
+  public Persona(String nombre) {
+	    super();
+	    this.nombre = nombre;
+	}
+  
+  public String getNombre() {
+	    return nombre;
+	}
+	  
+	public void setNombre(String nombre) {
+	    this.nombre = nombre;
+	}
+  
+	  public abstract int  correr() ;
 }
