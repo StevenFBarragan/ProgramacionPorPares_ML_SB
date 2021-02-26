4a7dff93e67d8c0019c752c98687bb3c9f123376
StevenFBarragan <64034066+StevenFBarragan@users.noreply.github.com>
2021-02-26 13:24:33 -0500

    Paquete: programacion.pares.Polimorfismo / Clase: Deportista
    En esta clase se agrego la extensión que indica que es una clase hija de la clase persona, se agrego el constructor que recibe como parámetro el nombre.
    También se agrego el método abstracto declarado en la clase Empleado.

--- ProgramacionPorPares_ML_SB/src/programacion/pares/Polimorfismo/Deportista.java
+++ ProgramacionPorPares_ML_SB/src/programacion/pares/Polimorfismo/Deportista.java
@@ -1,5 +1,15 @@
 package programacion.pares.Polimorfismo;
 
-public class Deportista {
+public class Deportista extends Persona{
+	
+  public Deportista(String nombre) {
+		super(nombre);
+	    // TODO Auto-generated constructor stub
+	}
 
+	@Override 
+	public int correr() {
+	    // TODO Auto-generated method stub
+		return 7;
+	}
 }
