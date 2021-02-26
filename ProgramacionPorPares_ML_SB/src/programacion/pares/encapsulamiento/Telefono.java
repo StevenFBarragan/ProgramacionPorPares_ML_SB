9b0c61cab03bbd7cbbcf551f31acc070363c653f
StevenFBarragan <64034066+StevenFBarragan@users.noreply.github.com>
2021-02-26 11:54:34 -0500

    Paquete: programacion.pares.encapsulamiento / Telefono
    Se agrego los atributos de la clase, el constructor, además, los métodos getters y setters

--- ProgramacionPorPares_ML_SB/src/programacion/pares/encapsulamiento/Telefono.java
+++ ProgramacionPorPares_ML_SB/src/programacion/pares/encapsulamiento/Telefono.java
@@ -2,6 +2,30 @@ package programacion.pares.encapsulamiento;
 
 public class Telefono {
 	
+	private String marca; 
+	private int capacidad;
+	
+	public String getMarca() {
+		return marca;
+	}
+	  
+	public void setMarca(String marca) {
+	    this.marca = marca;
+	}
+	  
+	public int getCapacidad() {
+	    return capacidad;
+	}
+	  
+	public void setCapacidad(int capacidad) {
+	    this.capacidad = capacidad;
+	}
+	 
+	public Telefono(String marca, int capacidad) {
+		super();
+	    this.marca = marca;
+	    this.capacidad = capacidad;
+	}
 	  
 }
 
