30646fefc0ffa3e6a36358805e2402129ac93837
StevenFBarragan <64034066+StevenFBarragan@users.noreply.github.com>
2021-02-26 12:23:23 -0500

    Paquete: programacion.pares.encapsulamiento / Clase: Tablet
    En la clase Tablet, agregamos:
    - Atributos
    - Constructor
    - Métodos getters y setters

--- ProgramacionPorPares_ML_SB/src/programacion/pares/encapsulamiento/Tablet.java
+++ ProgramacionPorPares_ML_SB/src/programacion/pares/encapsulamiento/Tablet.java
@@ -1,5 +1,30 @@
 package programacion.pares.encapsulamiento;
 
 public class Tablet {
+ 
+   private String marca;
+	 private int capacidad;
+	  
+	 public Tablet(String marca, int capacidad) {
+		    super();
+		    this.marca = marca;
+		    this.capacidad = capacidad;
+	 }
+	 
+	 public String getMarca() {
+	    return marca;
+	 }
+	  
+	 public void setMarca(String marca) {
+	    this.marca = marca; 
+	 }
+	  
+	 public int getCapacidad() {
+	    return capacidad;
+	 }
+	  
+	 public void setCapacidad(int capacidad) {
+	    this.capacidad = capacidad;
+	 }
 
 }
