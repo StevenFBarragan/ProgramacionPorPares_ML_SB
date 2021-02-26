56858eeef1c56e1d93a162c894ea82a2f07d0c59
StevenFBarragan <64034066+StevenFBarragan@users.noreply.github.com>
2021-02-26 12:33:10 -0500

    Paquete: programacion.pares.encapsulamiento / Clase: Bateria
    En la clase Bateria, procedí a agregar lo siguiente:
    - Atributos
    - El constructor
    - Métodos getters y setters

--- ProgramacionPorPares_ML_SB/src/programacion/pares/encapsulamiento/Bateria.java
+++ ProgramacionPorPares_ML_SB/src/programacion/pares/encapsulamiento/Bateria.java
@@ -1,5 +1,20 @@
 package programacion.pares.encapsulamiento;
 
 public class Bateria {
+  
+  private int capacidad;
+  
+  public Bateria(int capacidad) {
+	    super();
+	    this.capacidad = capacidad;
+	}
+	  
+	public int getCapacidad() {
+	    return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+	    this.capacidad = capacidad;
+	}	
 
 }
